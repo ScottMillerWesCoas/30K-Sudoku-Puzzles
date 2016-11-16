a2fe78b7ffdf9627539d9c8183e3ec511a4d5d2e
ScottMillerWesCoas <scott.t.miller1@gmail.com>
2016-11-16 07:39:02 -0800

    commented, cleaned code

--- server.js
+++ server.js
@@ -8,27 +8,25 @@ var fs = require('fs');
 
 app.use(express.static(path.join(__dirname, './'))); 
 
+
+//Use bodyParser so player can enter puzzle ID as URL to retreive specific puzzle 
 app.use(bodyParser.urlencoded({extended: true})); 
 app.use(cookieParser()); 
 
 
 
 app.get('/', function(req, res){
-        console.log('in sudoku serve file'); 
-        //so res.redirect as opposed to res.sendFile(which returns a 'no such file type error')
-        res.redirect('./index.html'); 
+    res.redirect('./views/index.html'); 
 }); 
 
 app.get('/sudoku', function(req, res){
-        //so res.redirect as opposed to res.sendFile(which returns a 'no such file type error')
-        res.redirect('./index.html'); 
+    res.redirect('./views/index.html'); 
 }); 
 
 app.get('/style.css', function(req, res){
     res.set('Content-Type', 'text/css; charset=UTF-8');
     res.status(200);
     res.sendFile(path.join(__dirname, './style.css'));
-
 }); 
 
 app.get('/puzzle', function(req, res){
@@ -37,19 +35,54 @@ app.get('/puzzle', function(req, res){
         var puzzles = data.split('\n'); 
         var thisPuzzleNum = Math.floor(Math.random() * 10000); 
         var thisPuzzle = puzzles[thisPuzzleNum]; 
-        //so res.redirect as opposed to res.sendFile(which returns a 'no such file type error')
+        //res.redirect as opposed to res.sendFile(which returns a 'no such file type error')
         res.redirect('/?board=' + thisPuzzle); 
     });  
 }); 
 
 
+//AJAX CALLS SO PUZZLES CAN REFRESH WITHOUT RE-LOADING BACKGROUND VIDEO
+//Data retreived as large text file, split on each line (each line in 10K line file is different puzzle)
+
+app.get('/easyData', function(req, res){
+    var puzzNum = Math.floor(Math.random() * 10000);  
+    fs.readFile('./puzzles/sudokuEasy.txt', "utf-8", function(err, data){ 
+        if (err) throw err;
+        var puzzles = data.split('\n'); 
+        var thisPuzzle = puzzles[puzzNum]; 
+        res.send(thisPuzzle); 
+    });  
+}); 
+
+app.get('/mediumData', function(req, res){
+    var puzzNum = Math.floor(Math.random() * 10000);  
+    fs.readFile('./puzzles/sudokuMedium.txt', "utf-8", function(err, data){ 
+        if (err) throw err;
+        var puzzles = data.split('\n'); 
+        var thisPuzzle = puzzles[puzzNum]; 
+        res.send(thisPuzzle); 
+    });  
+}); 
+
+app.get('/hardData', function(req, res){
+    var puzzNum = Math.floor(Math.random() * 10000);  
+    fs.readFile('./puzzles/sudokuHard.txt', "utf-8", function(err, data){ 
+        if (err) throw err;
+        var puzzles = data.split('\n'); 
+        var thisPuzzle = puzzles[puzzNum]; 
+        res.send(thisPuzzle); 
+    });  
+}); 
+
+
+
+//User can code puzzle # into URL to bring up a specific puzzle
 app.get('/puzzle/easy/:id', function(req, res){
     var key = req.params.id; 
     fs.readFile('./puzzles/sudokuEasy.txt', "utf-8", function(err, data){ 
         if (err) throw err;
         var puzzles = data.split('\n'); 
         var thisPuzzle = puzzles[key]; 
-        //so res.redirect as opposed to res.sendFile(which returns a 'no such file type error')
         res.redirect('/?board=' + thisPuzzle); 
     });  
 });  
@@ -73,41 +106,6 @@ app.get('/puzzle/hard/:id', function(req, res){
 }); 
 
 
-//AJAX CALLS SO PUZZLES CAN REFRESH WITHOUT RE-LOADING BACKGROUND VIDEO
-
-app.get('/easyData', function(req, res){
-    var puzzNum = Math.floor(Math.random() * 10000);  
-    fs.readFile('./puzzles/sudokuEasy.txt', "utf-8", function(err, data){ 
-        if (err) throw err;
-        var puzzles = data.split('\n'); 
-        var thisPuzzle = puzzles[puzzNum]; 
-        res.send(thisPuzzle); 
-    });  
+app.listen(3090, function(){
+    console.log('listening on 3090');
 }); 
-
-app.get('/mediumData', function(req, res){
-    var puzzNum = Math.floor(Math.random() * 10000);  
-    fs.readFile('./puzzles/sudokuMedium.txt', "utf-8", function(err, data){ 
-        if (err) throw err;
-        var puzzles = data.split('\n'); 
-        var thisPuzzle = puzzles[puzzNum]; 
-        //so res.redirect as opposed to res.sendFile(which returns a 'no such file type error')
-        res.send(thisPuzzle); 
-    });  
-}); 
-
-app.get('/hardData', function(req, res){
-    var puzzNum = Math.floor(Math.random() * 10000);  
-    fs.readFile('./puzzles/sudokuHard.txt', "utf-8", function(err, data){ 
-        if (err) throw err;
-        var puzzles = data.split('\n'); 
-        var thisPuzzle = puzzles[puzzNum]; 
-        //so res.redirect as opposed to res.sendFile(which returns a 'no such file type error')
-        res.send(thisPuzzle); 
-    });  
-}); 
-
-
-
-
-app.listen(3090, function(){console.log('listening on 3090');}); 
